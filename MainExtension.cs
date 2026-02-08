6ee58100414b0cb3001c2d6aca1fcb1a935ae685
JuliotLeonce <juliotleonce123@gmail.com>
2026-02-08 23:31:35 +0300

    Refactor: Move Swagger

--- MainExtension.cs
+++ MainExtension.cs
@@ -1,9 +1,29 @@
+using Microsoft.OpenApi;
+
 namespace poketra_vyrt_api;
 
 public static class MainExtension
 {
-    public static IServiceCollection AddSomethings(this IServiceCollection services)
+    public static IServiceCollection AddSwaggerSetup(this IServiceCollection services)
     {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo() { Title = "Poketra Vyrt API", Version = "v1" });
+
+            options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Description = "JWT Authorization header using the Bearer scheme."
+            });
+
+            options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+            {
+                [new OpenApiSecuritySchemeReference("bearer", document)] = []
+            });
+        });
         return services;
     }
 }
